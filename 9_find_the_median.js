8d42b6d265a9a3ef6af118b796990e5b3125aae0
tferguson1028 <tferguson1028@outlook.com>
2023-10-26 14:44:44 -0400

    Updated median formula to be the actual formula

--- 9_find_the_median.js
+++ 9_find_the_median.js
@@ -4,6 +4,6 @@ const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,
 
 nums.sort();
 if(nums.length%2 == 0)
-  console.log((nums[Math.floor(nums.length/2)]+nums[Math.floor(nums.length/2)+1])/2);
+  console.log((nums[Math.floor(nums.length/2)]+nums[Math.floor((nums.length+1)/2)])/2);
 else
-  console.log(nums[Math.floor(nums.length/2)]);
+  console.log(nums[Math.floor((nums.length+1)/2)]);
